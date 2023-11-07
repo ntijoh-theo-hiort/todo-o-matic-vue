75822ed506c0700d1353092fd6c819abe6759fe6
ntijoh-theo-hiort <theo.hiort@elev.ga.ntig.se>
2023-11-07 12:56:39 +0100

    copied from intro
    using quick vue intro project as a template

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+function logEvent(e : Event) {
+  console.log(e)
+}
+
+const messages = ref([
+  {id: 1, text: "Hello"},
+  {id: 2, text: "Wassup"},
+  {id: 3, text: "Wazzaaaaa"}
+])
+</script>
+
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  
+  <HelloWorld v-for="message in messages" :key="message.id" id="${message.id.toString()}" @updated="logEvent" :msg="message.text" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+
+const emit = defineEmits(['updated'])
+
+function emitUpdate(e : Event) {
+  e.preventDefault()
+  emit('updated', count.value)
+}
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <form @submit="emitUpdate" action="#">
+    <button>show current value</button>
+  </form>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
